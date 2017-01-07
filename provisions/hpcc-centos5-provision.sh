37fad34c15bd1f9e65bb3cae492dae0ff2f9ee78
root <root@hpccsystems-dev-el7-chefserver.novalocal>
2017-01-07 02:50:34 +0000

    Install hiredis from local file server. Also update el5 provision script

--- provisions/hpcc-centos5-provision.sh
+++ provisions/hpcc-centos5-provision.sh
@@ -55,33 +55,12 @@ then
   echo "${FILE_SERVER}	file-server.novalocal" >> /etc/hosts
 fi
 
-# Move /usr/local to /mnt/disk1/
-#-------------------------------
-if [ ! -d /mnt/disk1/usr/local ]
-then
-  mkdir -p /mnt/disk1/usr
-  mv /usr/local /mnt/disk1/usr/
-  ln -s /mnt/disk1/usr/local /usr/local
-fi
-
-# Move /opt to /mnt/disk1/
-#-------------------------------
-if [ ! -d /mnt/disk1/opt ]
-then
-   if [ -d /opt ]
-   then
-      mv /opt /mnt/disk1/
-   else
-      mkdir -p /mnt/disk1/opt
-   fi
-   ln -s /mnt/disk1/opt /opt
-fi
-
 # Move /tmp to /mnt/disk1/
 #-------------------------------
 if [ ! -d /mnt/disk1/tmp ]
 then
-  mv /tmp /mnt/disk1/
+  mkdir -p /mnt/disk1/tmp
+  rm -rf /tmp
   ln -s /mnt/disk1/tmp /tmp
 fi
 
@@ -94,72 +73,6 @@ then
   ln -s /mnt/disk1/Downloads /Downloads
 fi
 
-# Install pre-requisite packages 
-#-------------------------------
-#yum update -y
-yum install -y gcc-c++ gcc make binutils-devel openldap-devel libicu-devel 
-yum install -y libxslt-devel libarchive-devel boost141-devel openssl-devel apr-devel apr-util-devel
-yum install -y hiredis-devel numactl-devel libevent-devel apr1-devel aprutil-devel 
-yum install -y python26-devel.x86_64 sqlite-devel libmemcached-devel memcached-devel tbb-devel redis
-yum install -y rpm-build git mysql-devel curl-devel
-
-
-yum install -y python26-devel.x86_64 java-1.6.0-openjdk-devel java-1.7.0-openjdk-devel
-# Recent Jenkins requires Java 1.7 or later
-update-alternatives --set java /usr/lib/jvm/jre-1.7.0-openjdk.x86_64/bin/java  
-
-# Install R 
-#-------------------------------
-yum install -y R-core-devel
-cd /Downloads
-scp -o StrictHostKeyChecking=no root@${FILE_SERVER}:/data3/software/R/Rcpp_0.12.1.tar.gz .
-R CMD INSTALL Rcpp_0.12.1.tar.gz
-scp -o StrictHostKeyChecking=no root@${FILE_SERVER}:/data3/software/R/RInside_0.2.12.tar.gz .
-R CMD INSTALL RInside_0.2.12.tar.gz
-
-# Add ANTLRA and graphviz
-#-----------------------------------------
-cd /Downloads
-scp -r -o StrictHostKeyChecking=no root@${FILE_SERVER}:/data3/software/antlr/ANTLR.tar.gz .
-tar -zxvf ANTLR.tar.gz -C /usr/local/
-scp -r -o StrictHostKeyChecking=no root@${FILE_SERVER}:/data3/software/graphviz/graphviz-2.26.3.tar.gz .
-tar -zxvf graphviz-2.26.3.tar.gz -C /usr/local/src/
-
-
-# Add Maven
-#-----------------------------------------
-maven_name=apache-maven-3.3.9
-cd /usr/local
-if [ ! -d $maven_name ]
-then 
-  scp -o StrictHostKeyChecking=no root@${FILE_SERVER}:/data3/software/apache/${maven_name}-bin.tar.gz .
-  tar -zxvf ${maven_name}-bin.tar.gz
-  rm -rf ${maven_name}-bin.tar.gz
-  [ -d maven ] && rm -rf maven
-  ln -s $maven_name maven
-  grep MAVEN_HOME /etc/profile 
-  if [ $? -ne 0 ]
-  then
-     echo "export MAVEN_HOME=/usr/local/maven" >> /etc/profile
-     echo "export PATH=\${MAVEN_HOME}/bin:\$PATH" >> /etc/profile
-  fi
-fi
-
-# Add hadoop
-#-------------------------------
-cd /usr/local
-if [ ! -e hadoop ]
-then
-  scp -r -o StrictHostKeyChecking=no root@${FILE_SERVER}:/data3/software/hadoop/hadoop-1.2.1.tar.gz /usr/local/
-  tar -zxvf hadoop-1.2.1.tar.gz
-  rm -rf hadoop-1.2.1.tar.gz
-
-  scp -r -o StrictHostKeyChecking=no root@${FILE_SERVER}:/data3/software/hadoop/hadoop-2.6.0.tar.gz /usr/local/
-  tar -zxvf hadoop-2.6.0.tar.gz
-  rm -rf hadoop-2.6.0.tar.gz
-  ln -s hadoop-2.6.0 hadoop
-fi
-
 # Configure Jenkins slaves
 #-------------------------------
 cd /mnt/disk1
@@ -170,90 +83,7 @@ then
 fi
 [ ! -e /jenkins ] &&  ln -s /mnt/disk1/jenkins /jenkins
 
-# Install Ruby, Puppet agent
-#-------------------------------
-yum install -y ruby puppet
-cd /etc/puppet
-grep "^[[:space:]]*server[[:space:]]*=[[:space:]]file-server"  puppet.conf
-if [ $? -ne 0 ]
-then
-   sed -i '/^[[:space:]]*postrun_command/a server=file-server.novalocal' puppet.conf
-fi
-
-# Install cmake
-#------------------------------
-expected_version=3.5.2
-cmake_path=$(which cmake)
-[ -n "$cmake_path" ] && cmake_version=$(cmake -version | head -n 1 | cut -d' ' -f3)
-if [ -z "$cmake_path" ] || [[ "$cmake_version" != "$expected_version" ]]
-then
-   cd /Downloads
-   scp -o StrictHostKeyChecking=no root@${FILE_SERVER}:/data3/software/cmake/cmake-${expected_version}-el5-x86_64.tar.gz .
-   tar -zxf cmake-${expected_version}-el5-x86_64.tar.gz 
-   rm -rf  cmake-${expected_version}-el5-x86_64.tar.gz 
-   cd  cmake-${expected_version}-Linux-x86_64
-   cp -r * /usr/local/
-fi
-
-
-# Install C++11 package
-#------------------------------
-wget http://people.centos.org/tru/devtools-2/devtools-2.repo -O /etc/yum.repos.d/devtools-2.repo
-yum install -y devtoolset-2-gcc devtoolset-2-binutils
-yum install -y devtoolset-2-gcc-gfortran devtoolset-2-gcc-c++
-yum install -y devtoolset-2-libatomic-devel devtoolset-2-gdb devtoolset-2-git devtoolset-2-gitk
-yum install -y  devtoolset-2-valgrind devtoolset-2-elfutils devtoolset-2-strace devtoolset-2-git-gui
-
-# Install bison 2.4.1 
-#------------------------------
-bison_version=$(bison -V | head -n 1 | grep 2.4.1 | cut -d' ' -f4)
-if [ -z "$bison_version" ] || [[ "$bison_version" <  "2.4.1" ]]
-then
-   cd /Downloads/
-   wget http://${FILE_SERVER}/data3/software/bison/bison-2.4.1-src.tar.gz
-   tar -zxf bison-2.4.1-src.tar.gz
-   cd bison-2.4.1-src
-   ./configure
-   make
-   make install
-fi
-
-# Install flex 2.5.35
-#------------------------------
-flex_version=$(flex -V | head -n 1 | cut -d' ' -f 3)
-if [ -z "$flex_version" ] || [ "$flex_version" = "2.5.4" ] # [[ "2.5.4" < "2.5.35" ]] doesn't work. since 3 < 4
-then
-   cd /Downloads/
-   wget http://${FILE_SERVER}/data3/software/flex/flex-2.5.35.tar.gz
-   tar -zxf flex-2.5.35.tar.gz
-   cd flex-2.5.35
-   ./configure
-   make
-   make install
-fi
-
-# Install tbb
-#------------------------------
-rpm -qa | grep tbb-devel
-if [ $? -ne 0 ]
-then
-   cd /Downloads/
-   wget http://${FILE_SERVER}/data3/software/tbb/tbb-2.2-2.20090809.el5.x86_64.rpm
-   wget http://${FILE_SERVER}/data3/software/tbb/tbb-devel-2.2-2.20090809.el5.x86_64.rpm
-   rpm -i tbb-2.2-2.20090809.el5.x86_64.rpm tbb-devel-2.2-2.20090809.el5.x86_64.rpm
-fi
-
-# gpg 
-#------------------------------
-su - centos -c "wget http://${FILE_SERVER}/data3/build/gpg/HPCCSystems.priv"
-su - centos -c "gpg --import HPCCSystems.priv"
-su - centos -c "rm -rf HPCCSystems.priv"
-
-# atlas
-#------------------------------
-yum install -y atlas-devel
-
-# This doesn't work. Take off --silent will see the error similar to this:
-#OpenSSL: error:14077410:SSL routines:SSL23_GET_SERVER_HELLO:sslv3 alert handshake failure
-curl --silent --location https://rpm.nodesource.com/setup | bash -
-yum install -y nodejs
+#/etc/resolv.conf
+# search openstacklocal novalocal
+#nameserver 10.121.146.70
+#nameserver 10.121.146.71
